97c12387cfc1157a32472e43aba62f3f5ed833ed
Denis_Fomenkov <tgimrv@ya.ru>
2021-09-25 14:06:26 +0300

    add user

---
+++ src/main/java/ru/leadersofdigital/dobro/entity/Role.java
@@ -0,0 +1,29 @@
+package ru.leadersofdigital.dobro.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
