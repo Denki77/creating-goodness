2b1724df318324a66c5a5588e5d97bd839ed8ca6
Denis_Fomenkov <tgimrv@ya.ru>
2021-09-25 15:56:18 +0300

    add drop table IF EXISTS *;

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,41 +1,50 @@
+drop table IF EXISTS users;
 
-create table IF NOT EXISTS users (
-    id                      bigserial primary key,
-    username                varchar(80) not null,
-    password                varchar(80) not null,
-    email                   varchar(80) unique,
-    shelter_id              bigint null,
-    created_at              timestamp default current_timestamp,
-    updated_at              timestamp default current_timestamp
+create table IF NOT EXISTS users
+(
+    id         bigint primary key,
+    username   varchar(80) not null,
+    password   varchar(80) not null,
+    email      varchar(80) unique,
+    shelter_id bigint      null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp
 );
+drop table IF EXISTS roles;
 
-create table IF NOT EXISTS roles (
-    id                      bigserial primary key,
-    name                    varchar(80) unique,
-    created_at              timestamp default current_timestamp,
-    updated_at              timestamp default current_timestamp
+create table IF NOT EXISTS roles
+(
+    id         bigint primary key,
+    name       varchar(80) unique,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp
 );
-
-create table IF NOT EXISTS users_roles (
-    user_id                 bigint not null references users (id),
-    role_id                 bigint not null references roles (id),
+drop table IF EXISTS users_roles;
+create table IF NOT EXISTS users_roles
+(
+    user_id bigint not null references users (id),
+    role_id bigint not null references roles (id),
     primary key (user_id, role_id)
 );
+drop table IF EXISTS cities;
 
-create table IF NOT EXISTS cities (
-    id                     bigserial primary key,
-    name                   varchar(80) null,
-    lat                    varchar(80) null,
-    lng                    varchar(80) null
+create table IF NOT EXISTS cities
+(
+    id   bigint primary key,
+    name varchar(80) null,
+    lat  varchar(80) null,
+    lng  varchar(80) null
 );
+drop table IF EXISTS shelters;
 
-create table IF NOT EXISTS shelters (
-    id                     bigserial primary key,
-    name                   varchar(80) null,
-    number                 bigint,
-    city_id                bigint,
-    created_at              timestamp default current_timestamp,
-    updated_at              timestamp default current_timestamp
+create table IF NOT EXISTS shelters
+(
+    id         bigint primary key,
+    name       varchar(80) null,
+    number     bigint,
+    city_id    bigint,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp
 );
 
 
