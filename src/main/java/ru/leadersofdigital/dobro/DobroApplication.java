f85ef174f5dd22dae7b66e7a1101f849e4582dd2
Denis_Fomenkov <tgimrv@ya.ru>
2021-09-28 00:18:25 +0300

    Доработка

--- src/main/java/ru/leadersofdigital/dobro/DobroApplication.java
+++ src/main/java/ru/leadersofdigital/dobro/DobroApplication.java
@@ -4,11 +4,18 @@ package ru.leadersofdigital.dobro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource("classpath:app.properties")
-public class DobroApplication {
+public class DobroApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DobroApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DobroApplication.class, args);
